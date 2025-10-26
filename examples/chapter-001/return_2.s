ba02c21b2052829c3f821fcde1e7a993d7bf80e8
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-10-26 14:16:36 -0300

    Add example of '.c' program and its corresponding '.s' program from Chapter 001.

---
+++ examples/chapter-001/return_2.s
@@ -0,0 +1,10 @@
+	.file	"return_2.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	movl	$2, %eax
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
