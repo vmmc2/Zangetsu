b621aded2f639645955ec5239d4630da7f4fa8df
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-11-11 19:56:20 -0300

    Add a comment to the 'main.cpp' file explaining how to use the Zangetsu executable according to what I want.

--- app/main.cpp
+++ app/main.cpp
@@ -10,7 +10,10 @@ int main(int argc, const char **argv) {
   // TODO: Separate the main program into two: One for personal use and another
   // to run the tests from the book.
   if (argc > 1) {
-    std::string file_path = std::string{argv[1]};
+    // If working by youself, use 'argv[1]'.
+    // If working with the tests from the 'writing-a-c-compiler-tests' repo, use
+    // 'argv[2]'.
+    std::string file_path = std::string{argv[2]};
 
     FileScanner file_scanner;
     std::string file_content = file_scanner.GetFileContent(file_path);
