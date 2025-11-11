190f5ccb3d85b73fde5842cd39b80e7f7137e9fd
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-11-10 21:24:27 -0300

    Add initial implementation of the 'FileScanner' class.

---
+++ src/file_scanner/file_scanner.cpp
@@ -0,0 +1,33 @@
+#include "file_scanner.hpp"
+
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+
+std::string FileScanner::GetFileContent(std::string file_path) {
+  std::filesystem::path source_code_file_path{file_path};
+
+  if (source_code_file_path.extension() != ".c") {
+    throw std::runtime_error{
+        "The Zangetsu compiler is a compiler for the C programming language. "
+        "Input files must have the '.c' extension.\n"};
+  }
+
+  std::ifstream file{file_path,
+                     std::ios::in | std::ios::binary | std::ios::ate};
+  if (!file) {
+    throw std::runtime_error{std::format(
+        "An error has happened while trying to open the file: {}.\n",
+        file_path)};
+  }
+
+  std::string file_content;
+  file_content.resize(file.tellg());
+  file.seekg(0, std::ios::beg);
+  file.read(file_content.data(), file_content.size());
+  file.close();
+
+  return file_content;
+}
---
+++ src/file_scanner/file_scanner.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+class FileScanner {
+public:
+  std::string GetFileContent(std::string file_path);
+};
