396a406828c59ca4375d2b8549f1f690c44638de
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-11-10 21:25:35 -0300

    Add changes to the 'main.cpp' file so I can check the functioning of the 'FileScanner' and 'Lexer' pipeline.

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
+#include "../src/compiler_utils/token.hpp"
+#include "../src/file_scanner/file_scanner.hpp"
 #include "../src/lexer/lexer.hpp"
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
+int main(int argc, const char **argv) {
+  if (argc > 1) {
+    std::string file_path = std::string{argv[1]};
+
+    FileScanner file_scanner;
+    std::string file_content = file_scanner.GetFileContent(file_path);
+
+    Lexer lexer(file_content);
+
+    try {
+      std::vector<Token> tokens = lexer.Lex();
+      for (auto token : tokens) {
+        std::cout << token << std::endl;
+      }
+    } catch (std::exception &e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
 
   return 0;
 }
