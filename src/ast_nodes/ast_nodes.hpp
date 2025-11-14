0289b6da69191c314329239d8d5471e4b3fe2087
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-11-14 02:45:41 -0300

    Add initial declaration of the AST nodes that will be used in the implementation of Chapter 1 (It is wrong as it is, I know).

--- src/ast_nodes/ast_nodes.hpp
+++ src/ast_nodes/ast_nodes.hpp
@@ -1,3 +1,30 @@
 #pragma once
 
-class AstNode{};
+#include <any>
+
+#include "../compiler_utils/token.hpp"
+
+class AstNode {
+public:
+};
+
+class ProgramNode {
+public:
+  AstNode *function_definition_;
+};
+
+class FunctionDefinitionNode {
+public:
+  Token function_identifier_;
+  AstNode *function_body_;
+};
+
+class ReturnStmt {
+public:
+  AstNode *expr_;
+};
+
+class ConstantExpr {
+public:
+  std::any value_;
+};
